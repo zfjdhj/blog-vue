ed39d47abeb7808a73671aadc9bb4f2d34d45dce
Administrator <320336328@qq.com>
2020-10-19 19:57:27 +0800

    fix custom.css

--- zfjsite/example-settings.py
+++ zfjsite/example-settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z#jk#sg-zg0e@^g7(c)t^@m_cmbeh09)0d!vz3s4!i2zzi35&c'
+SECRET_KEY = 'XXXXXXXXXXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
